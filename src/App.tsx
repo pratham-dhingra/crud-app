03b500337fbd6a7a2a77e16d0d9c183bdfa4a259
Pratham Dhingra <prathamdhingra02@gmail.com>
2024-09-06 14:29:06 +0530

    Skeleton card added

--- src/App.tsx
+++ src/App.tsx
@@ -26,29 +26,19 @@ function App() {
   return (
     <>
       <main className="flex h-screen w-screen flex-col overflow-y-scroll bg-slate-900">
-        {loading && (
-          <>
-            <section className="flex h-full w-full flex-col p-6">
-              <div className="flex justify-between gap-2">
-                <h1 className="text-4xl font-bold text-white">Loading </h1>
-                <CircularProgress color="secondary" />
-              </div>
-            </section>
-          </>
-        )}
-        {!loading && (
-          <section className="flex h-full w-full flex-col p-6">
-            <div className="flex justify-between gap-2">
-              <h1 className="text-4xl font-bold text-white">Users</h1>
-              <div />
-            </div>
-            <div className="grid w-full grid-cols-1 gap-4 p-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-8 xl:grid-cols-4">
-              {users.map((user) => {
-                return <UserCard key={user.id} loading={loading} user={user} />;
-              })}
-            </div>
-          </section>
-        )}
+        <section className="flex h-full w-full flex-col p-6">
+          <div className="flex justify-between gap-2">
+            <h1 className="text-4xl font-bold text-white">
+              {loading ? "Loading" : "Users"}
+            </h1>
+            {loading ? <CircularProgress color="secondary" /> : <div />}
+          </div>
+          <div className="grid w-full grid-cols-1 gap-4 p-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-8 xl:grid-cols-4">
+            {users.map((user) => {
+              return <UserCard key={user.id} loading={loading} user={user} />;
+            })}
+          </div>
+        </section>
       </main>
     </>
   );
--- src/UserCard.tsx
+++ src/UserCard.tsx
@@ -4,6 +4,7 @@ import {
   CardActions,
   CardContent,
   IconButton,
+  Skeleton,
   Typography,
 } from "@mui/material";
 import { Link } from "react-router-dom";
@@ -20,57 +21,84 @@ const UserCard = (props: Props) => {
       <Card variant="outlined" className="h-[230px] w-full">
         <CardContent>
           <div className="flex gap-2">
-            <Typography
-              gutterBottom
-              sx={{ color: "text.secondary", fontSize: 14 }}
-            >
-              {props.user.company.name}
-              {", "}
-              {props.user.company.catchPhrase}
-            </Typography>
+            {props.loading ? (
+              <Skeleton variant="rectangular" className="mb-2 h-4 w-full" />
+            ) : (
+              <Typography
+                gutterBottom
+                sx={{ color: "text.secondary", fontSize: 14 }}
+              >
+                {props.user.company.name}
+                {", "}
+                {props.user.company.catchPhrase}
+              </Typography>
+            )}
           </div>
           <Typography variant="h5" component="div">
-            {props.user.name}
+            {props.loading && (
+              <Skeleton variant="rectangular" className="mb-2 h-6 w-full" />
+            )}
+            {!props.loading && props.user.name}
           </Typography>
           <Typography
             // gutterBottom
             sx={{ color: "text.secondary", fontSize: 14 }}
           >
-            Email: {props.user.email}
+            {props.loading && (
+              <Skeleton variant="rectangular" className="mb-2 h-4 w-full" />
+            )}
+            {!props.loading && <>Email: {props.user.email}</>}
           </Typography>
           <Typography
             // gutterBottom
             sx={{ color: "text.secondary", fontSize: 14 }}
           >
-            Phone: {props.user.phone}
+            {props.loading && (
+              <Skeleton variant="rectangular" className="mb-2 h-4 w-full" />
+            )}
+            {!props.loading && <>Phone: {props.user.phone}</>}
           </Typography>
           <Typography variant="body2">
-            {props.user.address.street}, {props.user.address.city}
+            {props.loading && (
+              <Skeleton variant="rectangular" className="mb-2 h-4 w-full" />
+            )}
+            {!props.loading && (
+              <>
+                {props.user.address.street}, {props.user.address.city}
+              </>
+            )}
           </Typography>
         </CardContent>
         <CardActions disableSpacing>
-          <Link to={`tel:${props.user.phone}`}>
-            <IconButton aria-label="Call User">
-              <Call />
-            </IconButton>
-          </Link>
-          <Link to={`https://${props.user.website}`}>
-            <IconButton aria-label="Website">
-              <Language />
-            </IconButton>
-          </Link>
-          <Link to={`mailto:${props.user.email}`}>
-            <IconButton aria-label="Email User">
-              <Email />
-            </IconButton>
-          </Link>
-          <Link
-            to={`https://www.google.com/maps?q=${props.user.address.geo.lat},${props.user.address.geo.lng}`}
-          >
-            <IconButton aria-label="Location">
-              <LocationOn />
-            </IconButton>
-          </Link>
+          {props.loading && (
+            <Skeleton variant="rounded" className="mb-2 h-8 w-full" />
+          )}
+          {!props.loading && (
+            <>
+              <Link to={`tel:${props.user.phone}`}>
+                <IconButton aria-label="Call User">
+                  <Call />
+                </IconButton>
+              </Link>
+              <Link to={`https://${props.user.website}`}>
+                <IconButton aria-label="Website">
+                  <Language />
+                </IconButton>
+              </Link>
+              <Link to={`mailto:${props.user.email}`}>
+                <IconButton aria-label="Email User">
+                  <Email />
+                </IconButton>
+              </Link>
+              <Link
+                to={`https://www.google.com/maps?q=${props.user.address.geo.lat},${props.user.address.geo.lng}`}
+              >
+                <IconButton aria-label="Location">
+                  <LocationOn />
+                </IconButton>
+              </Link>
+            </>
+          )}
         </CardActions>
       </Card>
     </>
